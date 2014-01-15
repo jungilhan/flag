9f16c82aafad619c511f1fd223973d2da954643b
Jungil Han <jungil.han@gmail.com>
2014-01-15 15:43:58 +0900

    릴리즈 signing 키 용 및 proguard 적용

--- Flag/src/main/java/com/bulgogi/flag/config/Constants.java
+++ Flag/src/main/java/com/bulgogi/flag/config/Constants.java
@@ -2,7 +2,7 @@ package com.bulgogi.flag.config;
 
 public class Constants {
     public static class Config {
-        public static final boolean DEBUG = true;
+        public static final boolean DEBUG = false;
         public static final boolean NEXT_FEATURE = false;
     }
 }
